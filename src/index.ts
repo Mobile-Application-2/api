9af28a8c5f0e5b27c731aaeee0e3e7037cf49652
Alfred-Onuada <aonuada5@gmail.com>
2024-05-14 00:37:50 +0100

    db connection setup

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ config();
 import express from 'express';
 import cors from 'cors';
 import compression from 'compression';
+import mongoose from 'mongoose';
+import generalRoutes from './routes/general.routes';
 
 const app = express();
 app.use(cors());
@@ -11,10 +13,17 @@ app.use(express.json());
 app.use(compression());
 
 const PORT = process.env.PORT || 5656;
+const DB_URI = process.env.DB_URI;
 
 async function main() {
-  app.get('', (_, res) => {
-    res.status(200).json({message: 'App is running'});
+  console.log('Connecting to DB');
+  await mongoose.connect(DB_URI as string);
+  console.log('DB connection established');
+
+  app.use('', generalRoutes);
+
+  app.get('*', (_, res) => {
+    res.status(404).json({message: 'Route not found'});
   });
 
   app.listen(PORT, () => console.log('App is now running'));
